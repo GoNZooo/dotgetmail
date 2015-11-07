582ab0994039e73d7dddb2a91461b3d20d3a0c68
Rickard Andersson <gonz@severnatazvezda.com>
2015-11-07 18:48:57 +0200

    Add archive-log.sh for archiving logs once per month

---
+++ archive-log.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+GETMAILDIR="$HOME/.config/getmail"
+LOGFILE="$GETMAILDIR/log"
+LOGARCHIVE="$GETMAILDIR/log-archive"
+
+MONTH=`date +%Y.%m`
+
+mkdir -p "$LOGARCHIVE"
+tar cvzf "$LOGARCHIVE/$MONTH.tar.gz"
+rm "$LOGFILE"
+touch "$LOGFILE"
